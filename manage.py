f0cdb1e12351b3f8a226d7cff18d2809c4537006
Petr Antonov <antpeter@gmail.com>
2017-09-04 18:57:05 +0300

    Test Setup

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 # manage.py
 
-
+import unittest
 from flask_script import Manager
 
 from project import app, db
@@ -16,6 +16,15 @@ def recreate_db():
     db.create_all()
     db.session.commit()
 
+
+@manager.command
+def test():
+    """Runs the tests without code coverage."""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
 
 if __name__ == '__main__':
     manager.run()
