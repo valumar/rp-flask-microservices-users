a6d20b7a396fae3689a79c0826dddf1c3bcb4532
Petr Antonov <antpeter@gmail.com>
2017-09-08 17:03:36 +0300

    Adding coverage

--- project/tests/test_config.py
+++ project/tests/test_config.py
@@ -2,6 +2,7 @@
 
 
 import unittest
+import os
 
 from flask import current_app
 from flask_testing import TestCase
@@ -22,7 +23,7 @@ class TestDevelopmentConfig(TestCase):
         self.assertFalse(current_app is None)
         self.assertTrue(
             app.config['SQLALCHEMY_DATABASE_URI'] ==
-            'postgres://postgres:postgres@users-db:5432/users_dev'
+            os.environ.get('DATABASE_URL')
         )
 
 
@@ -38,7 +39,7 @@ class TestTestingConfig(TestCase):
         self.assertFalse(app.config['PRESERVE_CONTEXT_ON_EXCEPTION'])
         self.assertTrue(
             app.config['SQLALCHEMY_DATABASE_URI'] ==
-            'postgres://postgres:postgres@users-db:5432/users_test'
+            os.environ.get('DATABASE_TEST_URL')
         )
 
 
